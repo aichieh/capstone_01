0cd0a632be307cf6dea7364e860b41f9037f8230
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-11 18:51:50 -0500

    Update Hypertension_app.py

--- Hypertension_app.py
+++ Hypertension_app.py
@@ -23,6 +23,78 @@ st.markdown("<h1 style='text-align: center'>Examining Hypertension Using Health 
 # Read the data
 df1 = pd.read_csv("./output/df_try.csv")
 
+st.sidebar.markdown("""
+Input your data here.
+""")
+gender= st.sidebar.slider('Sex', ('NO', 'YES'))
+age= st.sidebar.slider('Age', 1, 100, 30)
+weight = st.sidebar.selectbox('Weight (lb)', 10.0, 400.0, 150.0)
+waist_circumference = st.sidebar.selectbox('Waist Circumference (inch)', 10.0, 80.0, 30.0)
+systolic_bp = st.sidebar.slider('Blood Pressure(upper value) (mmHg)', 100.0, 250.0, 120.0)
+BMI = st.sidebar.slider('BMI (kg/m^2)', 15.0, 70.0, 23.0)
+
+hypertension = st.sidebar.selectbox('Have hypertension', ('NO', 'YES'))
+take_HTN_medicine = st.sidebar.selectbox('Takes BP medicines', ('NO', 'YES'))
+high_cholesterol = st.sidebar.slider('Have high cholesterol', ('NO', 'YES'))
+take_HCL_medicine = st.sidebar.selectbox('Takes cholesterol medicines', ('NO', 'YES'))
+diabetes = st.sidebar.selectbox('Have diabetes', ('NO', 'YES'))
+stroke = st.sidebar.selectbox('Had any prevalent Stroke', ('NO', 'YES'))
+heart_failure = st.sidebar.selectbox('Had any heart failure', ('NO', 'YES'))
+coronary_heart_disease = st.sidebar.selectbox('Had any heart failure', ('NO', 'YES'))
+angina = st.sidebar.selectbox('Had any angina', ('NO', 'YES'))
+heart_attack = st.sidebar.selectbox('Had any heart attack', ('NO', 'YES'))
+
+#60_sec_pulse = st.sidebar.slider('Heart Rate (per minute)', 30.0, 130.0, 40.0)
+#glucose = st.sidebar.slider('Glucose (mg/dl)', 100.0, 500.0, 110.0)
+#education = st.sidebar.selectbox('Education', ('10th pass', '12th pass/Diploma', 'Bachelors', 'Masters or Higher'))
+#current_smoker = st.sidebar.selectbox('Current Smoker', ('NO', 'YES'))
+#cigsPerDay = st.sidebar.slider('Cigarettes per Day', 0, 100, 20)
+
+def value(lst, string):
+    for i in range(len(lst)):
+        if lst[i] == string:
+            return i
+sex=['Female', 'Male']
+#edu=['10th pass', '12th pass/Diploma', 'Bachelors', 'Masters or Higher']
+yn=['NO', 'YES']
+
+st.markdown("<h3 style='text-align: center; color:#4dffa6;'>Update your details in the sidebar</h3>", unsafe_allow_html = True)
+st.markdown("<h3 style='text-align: center; color:#4dffa6;'><----</h3>", unsafe_allow_html = True)
+if st.sidebar.button('Submit'):
+        data = {'gender': value(sex, male),
+                'age': age,
+                'weight': weight,
+                'waist_circumference': waist_circumference,
+                'systolic_bp': systolic_bp,
+                'BMI': BMI,
+                'hypertension': value(yn, hypertension),
+                'take_HTN_medicine': value(yn, hypertension),
+                'high_cholesterol': value(yn, high_cholesterol),
+                'take_HCL_medicine': value(yn, take_HCL_medicine),
+                'diabetes': value(yn, diabetes),
+                'stroke': value(yn, stroke),
+                'heart_failure': value(yn, heart_failure),
+                'coronary_heart_disease': value(yn, coronary_heart_disease),
+                'angina': value(yn, angina),
+                'heart_attack': value(yn, heart_attack)}
+#'heartRate': heart_rate,
+#'glucose': glucose}
+#'education': value(edu, education),
+#'currentSmoker': value(yn, current_smoker),
+#'cigsPerDay': cigsPerDay,  
+        features = pd.DataFrame(data, index=[0])
+
+        st.markdown("<h2 style='text-align: center; color:#000066;'>Data gathered........</h2>", unsafe_allow_html = True)
+        st.markdown("<h2 style='text-align: center; color:#000066;'>Processing Results........</h2>", unsafe_allow_html = True)
+        # Reads in saved classification model
+        load_clf = pickle.load(open('stroke.pkl', 'rb'))
+        
+        # Apply model to make predictions
+        prediction = load_clf.predict(features)
+        prediction_proba = load_clf.predict_proba(features).reshape(2,)
+        yes = prediction_proba[1]
+        no = prediction_proba[0]
+        
 # Creating the container for the first plot
 #with st.beta_expander('Stroke Prediction'):
 
@@ -30,14 +102,6 @@ df1 = pd.read_csv("./output/df_try.csv")
 #    cat_option = st.selectbox('Select a feature to examine', cat_cols, key='cat_cols1')
 
 # The code to run the first plot
-target = 'stroke'
-X = df1.drop([target], axis=1)
-y = df1[target]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-lg=LogisticRegression(max_iter = 30000)
-lg.fit(X_train,y_train)
 
 #Predict training set:
 y_pred = lg.predict(X_test)
@@ -65,57 +129,6 @@ result['AUC'] = [round(roc_auc_score(y_test, lg_probs),2)]
 
 # Explaination of the features displays along with the graph
 st.markdown('**Explaination of the feature selected:**')
-
-st.sidebar.markdown("""
-Input your data here.
-""")
-male = st.sidebar.selectbox('Sex', ('Female', 'Male'))
-age= st.sidebar.slider('Age', 5.0, 100.0, 30.0)
-education = st.sidebar.selectbox('Education', ('10th pass', '12th pass/Diploma', 'Bachelors', 'Masters or Higher'))
-current_smoker = st.sidebar.selectbox('Current Smoker', ('NO', 'YES'))
-cigsPerDay = st.sidebar.slider('Cigarettes per Day', 0, 100, 20)
-BPMeds = st.sidebar.selectbox('Takes BP medicines', ('NO', 'YES'))
-prevstrk = st.sidebar.selectbox('Had any prevalent Stroke', ('NO', 'YES'))
-prevhyp = st.sidebar.selectbox('Had any prevalent Hypertension', ('NO', 'YES'))
-diabetes = st.sidebar.selectbox('Have diabetes', ('NO', 'YES'))
-chol = st.sidebar.slider('Cholesterol (mg/dl)', 0.0, 700.0, 230.0)
-highbp = st.sidebar.slider('Blood Pressure(upper value) (mmHg)', 100.0, 250.0, 120.0)
-lowbp = st.sidebar.slider('Blood Pressure(Lower Value) (mmHg)', 50.0, 180.0, 80.0)
-BMI = st.sidebar.slider('BMI (kg/m^2)', 15.0, 70.0, 23.0)
-heart_rate = st.sidebar.slider('Heart Rate (per minute)', 30.0, 130.0, 40.0)
-glucose = st.sidebar.slider('Glucose (mg/dl)', 100.0, 500.0, 110.0)
-
-st.markdown("<h3 style='text-align: center; color:#4dffa6;'>Update your details in the sidebar</h3>", unsafe_allow_html = True)
-st.markdown("<h3 style='text-align: center; color:#4dffa6;'><----</h3>", unsafe_allow_html = True)
-if st.sidebar.button('Submit'):
-        data = {'male': value(sex, male),
-                'age': age,
-                'education': value(edu, education),
-                'currentSmoker': value(yn, current_smoker),
-                'cigsPerDay': cigsPerDay,
-                'BPMeds': value(yn, BPMeds),
-                'prevalentStroke': value(yn, prevstrk),
-                'prevalentHyp': value(yn, prevhyp),
-                'diabetes': value(yn, diabetes),
-                'totChol': chol,
-                'sysBP': highbp,
-                'diaBP': lowbp,
-                'BMI': BMI,
-                'heartRate': heart_rate,
-                'glucose': glucose}
-        features = pd.DataFrame(data, index=[0])
-
-        st.markdown("<h2 style='text-align: center; color:#000066;'>Data gathered........</h2>", unsafe_allow_html = True)
-        st.markdown("<h2 style='text-align: center; color:#000066;'>Processing Results........</h2>", unsafe_allow_html = True)
-        # Reads in saved classification model
-        load_clf = pickle.load(open('stroke.pkl', 'rb'))
-        
-        # Apply model to make predictions
-        prediction = load_clf.predict(features)
-        prediction_proba = load_clf.predict_proba(features).reshape(2,)
-        yes = prediction_proba[1]
-        no = prediction_proba[0]
-        
         
         
         
