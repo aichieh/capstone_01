d87e013250f9f7e6ea7e6c8fa5e5a775448b133b
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-12 09:16:41 -0500

    Update Hypertension_app.py

--- Hypertension_app.py
+++ Hypertension_app.py
@@ -111,30 +111,30 @@ prediction_proba = model.predict_proba(features).reshape(2,)
 risk = (prediction_proba[1]*100).round(2) 
 #st.write(yes, " %")
 
-def userData():
-    return []
+#def userData():
+#    return []
 
 #@st.cache(allow_output_mutation=True)
-def delta(l, p):
-    if len(l) == 0:
-        l.extend([0, round(p*100, 1)])
-        d = 0
-    else:
-        l.pop(0)
-        l.append(round(p*100, 1))
-        d = l[1] - l[0]
-    return d
+#def delta(l, p):
+#    if len(l) == 0:
+#        l.extend([0, round(p*100, 1)])
+#        d = 0
+#    else:
+#        l.pop(0)
+#        l.append(round(p*100, 1))
+#        d = l[1] - l[0]
+#    return d
 
 #col1, col2 = st.columns(2)
-st.metric(
-    label="Risk of Stroke", 
-    value= str(risk) + " %", 
-    delta=str(delta(userData(), risk)) + " percentage points", 
-    help="""
-    The change in percentage points is displayed below.
-    """,
-    delta_color ="inverse"
-)
+#st.metric(
+#    label="Risk of Stroke", 
+#    value= str(risk) + " %", 
+#    delta=str(delta(userData(), risk)) + " percentage points", 
+#    help="""
+#    The change in percentage points is displayed below.
+#    """,
+#    delta_color ="inverse"
+#)
 
 # Reads in saved classification model
 #model2 = pickle.load(open('htn.pkl', 'rb'))
