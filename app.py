cc2f2e3dea5b557585b9a31f0ae07a09984caee8
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-11 19:19:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,55 +29,9 @@ df1 = pd.read_csv("./output/df_try.csv")
 # Creating a selectbox dropdown with the categorical features to choose from
 #    cat_option = st.selectbox('Select a feature to examine', cat_cols, key='cat_cols1')
 
-# The code to run the first plot
-target = 'stroke'
-X = df1.drop([target], axis=1)
-y = df1[target]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-lg=LogisticRegression(max_iter = 30000)
-lg.fit(X_train,y_train)
-
-#Predict training set:
-y_pred = lg.predict(X_test)
-
-CM = confusion_matrix(y_test, y_pred)
-TN = CM[0][0]
-FN = CM[1][0]
-TP = CM[1][1]
-FP = CM[0][1]
-
-result=pd.DataFrame()
-# Sensitivity, hit rate, recall, or true positive rate
-result['TPR'] = [round(TP/(TP+FN),2)]
- # Specificity or true negative rate
-result['TNR'] = [round(TN/(TN+FP),2) ]
-        # Fall out or false positive rate
-result['FPR'] = [round(FP/(FP+TN),2)]
-         # False negative rate
-result['FNR'] = [round(FN/(TP+FN),2)]
-
-lg_probs = lg.predict_proba(X_test)
-        # keep probabilities for the positive outcome only
-lg_probs = lg_probs[:, 1]
-result['AUC'] = [round(roc_auc_score(y_test, lg_probs),2)]
-
-gbc_auc = roc_auc_score(y_test, lg_probs)
-gbc_fpr, gbc_tpr, gbc_thresholds = roc_curve(y_test, lg_probs)
-plt.plot(gbc_fpr, gbc_tpr, label='ROC Curve (AUC = %0.2f)' % (gbc_auc))
-plt.plot([0, 1], [0, 1], linestyle='--', color='red', label='No skill')   
-plt.plot([0, 0, 1], [0, 1, 1], linestyle=':', color='green', label='Perfect Classifier')
-plt.xlim([-0.05, 1.05])
-plt.ylim([-0.05, 1.05])
-plt.xlabel('False positive rate')
-plt.ylabel('True positive rate')
-plt.legend(loc="lower right")
-plt.show()
 
 st.sidebar.markdown("""
 Input your data here .
-It is already set to normal values.
 """)
 male = st.sidebar.selectbox('Sex', ('Female', 'Male'))
 age= st.sidebar.slider('Age', 5.0, 100.0, 30.0)
@@ -118,7 +72,7 @@ if st.sidebar.button('Submit'):
         st.markdown("<h2 style='text-align: center; color:#000066;'>Data gathered........</h2>", unsafe_allow_html = True)
         st.markdown("<h2 style='text-align: center; color:#000066;'>Processing Results........</h2>", unsafe_allow_html = True)
         # Reads in saved classification model
-        load_clf = pickle.load(open('heart_disease.pkl', 'rb'))
+        load_clf = pickle.load(open('stroke.pkl', 'rb'))
         
         # Apply model to make predictions
         prediction = load_clf.predict(features)
