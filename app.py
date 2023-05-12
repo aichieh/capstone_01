ba6fece072ae1a0787d2eea3840525e1511be245
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-12 00:23:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ take_HTN_medicine = df['take_HTN_medicine']
 high_cholesterol = df['high_cholesterol']
 take_HCL_medicine = df['take_HCL_medicine']
 diabetes = df['diabetes']
-stroke = df['stroke']
+#stroke = df['stroke']
 heart_failure = df['heart_failure']
 CAD = df['CAD']
 angina = df['angina']
@@ -62,7 +62,7 @@ take_HTN_medicine_choice = st.sidebar.selectbox('Takes BP medicines', ('NO', 'YE
 high_cholesterol_choice = st.sidebar.selectbox('Have high cholesterol', ('NO', 'YES'))
 take_HCL_medicine_choice = st.sidebar.selectbox('Takes cholesterol medicines', ('NO', 'YES'))
 diabetes_choice = st.sidebar.selectbox('Have diabetes', ('NO', 'YES'))
-stroke_choice = st.sidebar.selectbox('Had any prevalent Stroke', ('NO', 'YES'))
+#stroke_choice = st.sidebar.selectbox('Had any prevalent Stroke', ('NO', 'YES'))
 heart_failure_choice = st.sidebar.selectbox('Had any heart failure', ('NO', 'YES'))
 CAD_choice = st.sidebar.selectbox('Had any coronary heart disease', ('NO', 'YES'))
 angina_choice = st.sidebar.selectbox('Had any angina', ('NO', 'YES'))
@@ -116,7 +116,7 @@ if st.sidebar.button('Submit'):
                 'high_cholesterol': value(yn, high_cholesterol_choice),
                 'take_HCL_medicine': value(yn, take_HCL_medicine_choice),
                 'diabetes': value(yn, diabetes_choice),
-                'stroke': value(yn, stroke_choice),
+                #'stroke': value(yn, stroke_choice),
                 'heart_failure': value(yn, heart_failure_choice),
                 'CAD': value(yn, CAD_choice),
                 'angina': value(yn, angina_choice),
