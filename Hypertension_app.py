adf633ced5ba24a97cfe2d1f97b8e27fa28b150b
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-11 19:11:29 -0500

    Update Hypertension_app.py

--- Hypertension_app.py
+++ Hypertension_app.py
@@ -131,32 +131,3 @@ if st.sidebar.button('Submit'):
         yes = prediction_proba[1]
         no = prediction_proba[0]
         
-
-        
-        
-        st.markdown("<h2 style='text-align: center; color:#99ffff;'><u>Prediction </u></h2>", unsafe_allow_html = True)
-        pred1, pred2, pred3 = st.beta_columns([12, 6, 14])
-        if prediction==0:
-            st.markdown("<h1 style='text-align: center; color:#006600;'>You don't have any heart problem.</h1>", unsafe_allow_html = True)
-            with pred1:
-                st.write("")
-            with pred2:
-                st.image("smile_emo.png")
-            with pred3:
-                st.write("")
-        else:
-            st.markdown("<h1 style='text-align: center; color:#cc0000;'>Go to a doctor.You may have heart problems.</h1>", unsafe_allow_html = True)
-            with pred1:
-                st.write("")
-            with pred2:
-                st.image("amb.png")
-            with pred3:
-                st.write("")
-        
-            
-        st.markdown("<h1 style='text-align: center; color:#99ffff;'><u>Prediction Probability</u></h1>", unsafe_allow_html = True)
-        fig,ax=plt.subplots(figsize=(10,8))
-        axes=plt.bar(['Chances of being healthy\n{} %'.format(no*100),'Chances of getting cardiac diseases\n{} %'.format(yes*100)], [no, yes])
-        axes[0].set_color('g')
-        axes[1].set_color('r')
-        st.pyplot(fig)
