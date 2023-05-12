07b22a467952e44b72db800ff3d07cfa922171be
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-12 01:02:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -137,6 +137,7 @@ if st.sidebar.button('Submit'):
         prediction = model.predict(features)
         prediction_proba = model.predict_proba(features).reshape(2,)
         yes = prediction_proba[1]
+        st.write(yes)
         no = prediction_proba[0]
  
 adjst=5
