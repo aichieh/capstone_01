6b116af628d582303f48423e6e6040901e2972c7
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-12 01:53:22 -0500

    Update Hypertension_app.py

--- Hypertension_app.py
+++ Hypertension_app.py
@@ -125,7 +125,7 @@ def delta(l, p):
 col1, col2, col3 = st.columns(3)
 col1.metric(
     label="Risk of Stroke", 
-    value=str(round(prediction_proba*100, 1)) + " %", 
+    value=str((round(prediction_proba*100, 1)) + " %"), 
     delta=str(round(delta(userData(), prediction_proba), 2)) + " percentage points"),
     delta_color ="inverse"
     )
