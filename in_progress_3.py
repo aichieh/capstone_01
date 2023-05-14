cb0558f2616c5e6f4a86723dbbbbfc85b97e1fb5
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-14 18:46:41 -0500

    Update in_progress_3.py

--- in_progress_3.py
+++ in_progress_3.py
@@ -83,6 +83,7 @@ sex=['Female', 'Male']
 yn=['NO', 'YES']
 
 tab1, tab2 = st.tabs(["Basic Health Information", "Chronic Disease Risk"])
+  
 
 if st.sidebar.button('Submit'):
         data = {'weight': weight_choice,
@@ -125,24 +126,11 @@ if st.sidebar.button('Submit'):
         yes = prediction_proba[1]
         no = prediction_proba[0]     
         
-        st.markdown("<h2 style='text-align: center; color:#99ffff;'><u>Prediction </u></h2>", unsafe_allow_html = True)
-        pred1, pred2, pred3 = st.beta_columns([12, 6, 14])
         if prediction==0:
-            st.markdown("<h1 style='text-align: center; color:#006600;'>You don't have any heart problem.</h1>", unsafe_allow_html = True)
-            with pred1:
-                st.write("")
-            with pred2:
-                st.image("smile_emo.png")
-            with pred3:
-                st.write("")
+            st.error('Warning! You have high risk of getting a heart attack!')
         else:
-            st.markdown("<h1 style='text-align: center; color:#cc0000;'>Go to a doctor.You may have heart problems.</h1>", unsafe_allow_html = True)
-            with pred1:
-                st.write("")
-            with pred2:
-                st.image("amb.png")
-            with pred3:
-                st.write("")
+            st.success('You have lower risk of getting a heart disease!')
+    
 ###-------------------------------------------------------------------------------------------------------------------------------------------------------------------####
 # BMI Meter
 def calculate_bmi(weight, height):
