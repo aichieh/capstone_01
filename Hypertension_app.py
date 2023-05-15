bcf6a06a537b62f93bd8bf17966a149d10af7323
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-14 23:04:15 -0500

    Update Hypertension_app.py

--- Hypertension_app.py
+++ Hypertension_app.py
@@ -136,19 +136,26 @@ st.metric(
     """,
     delta_color ="inverse"
 )
-#######Additional Information##################
+#######BMI Indicator##################
 
 @st.cache(allow_output_mutation=True)
-def assesBMI(BMI, AGE):
-    if BMI > 45 and AGE > 75:
+def calculate_bmi(weight, height):
+    weight_kg = weight*0.454
+    height_m = (height*2.54)/100
+    bmi = weight_kg / (height_m ** 2)
+    return bmi
+
+@st.cache(allow_output_mutation=True)
+def assesBMI(BMI):
+    if BMI > 45:
         inf = """
         Note: Information is unreliable.
-        BMI > 45 and age > 75.
+        BMI > 45.
         """
     elif BMI <= 10:
         inf = "BMI level:\nBMI too low"
-    elif BMI < 18.5 and BMI > 10:
-        inf = "BMI level:\nShortweight"
+    elif BMI < 18.5:
+        inf = "BMI level:\nUnderweight"
     elif BMI >= 18.5 and BMI < 25:
         inf = "BMI level:\nNormal Weight"
     elif BMI >= 25 and BMI < 30:
@@ -161,7 +168,37 @@ def assesBMI(BMI, AGE):
         inf = "BMI level:\nExtreme Obesity"
     return inf
 
-st.text(assesBMI(BMI_choice, age_choice))
+def create_bmi_gauge(value):
+    fig = go.Figure()
+    fig.add_trace(go.Indicator(
+        mode = "gauge+number",
+        value = value,
+        domain = {'x': [0, 1], 'y': [0, 1]},
+        title = {'text': "BMI"},
+        gauge = {'axis': {'range': [None, 40]},
+                 'bar': {'color': "darkblue"},
+                 'steps' : [
+                     {'range': [0, 18.5], 'color': 'lightgray'},
+                     {'range': [18.5, 24.9], 'color': 'green'},
+                     {'range': [24.9, 29.9], 'color': 'yellow'},
+                     {'range': [29.9, 40], 'color': 'red'}],
+                 'threshold' : {'line': {'color': "black", 'width': 4}, 'thickness': 0.75, 'value': 30}}))
+
+    fig.update_layout(height=200, margin=dict(l=10, r=10, t=10, b=10))
+    return fig
+
+# Streamlit App
+st.title("BMI Indicator Gauges")
+
+weight = st.number_input("Enter your weight (in lb):", min_value=0.0)
+height = st.number_input("Enter your height (in inch):", min_value=0.0)
+
+if st.button("Calculate BMI"):
+    bmi = calculate_bmi(weight, height)
+    st.write("Your BMI:", bmi)
+    st.write(assesBMI(bmi))
+    # Display BMI gauge for the submitted entry
+    st.plotly_chart(create_bmi_gauge(bmi))
 
 # BMI Meter
 #@st.cache(allow_output_mutation=True)
