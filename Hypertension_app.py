98b26aed52b905be20609dd3f31c285f2784d3e3
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-14 22:00:52 -0500

    Update Hypertension_app.py

--- Hypertension_app.py
+++ Hypertension_app.py
@@ -108,7 +108,7 @@ model = pickle.load(open('stroke_adult.pkl', 'rb'))
 prediction = model.predict(features)
 prediction_proba = model.predict_proba(features).reshape(2,)
 #st.write("Risk of Hypertension") 
-risk = prediction_proba[1]*100
+risk = prediction_proba[1]
 #st.write(risk, " %")
 
 @st.cache(allow_output_mutation=True)
@@ -129,7 +129,7 @@ def delta(l, p):
 #col1, col2 = st.columns(2)
 st.metric(
     label="Risk of Stroke", 
-    value= str(risk) + " %", 
+    value= str(risk*100) + " %", 
     delta=str(delta(userData(), risk)) + " percentage points", 
     help="""
     The change in percentage points is displayed below.
