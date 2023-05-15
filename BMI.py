ef414c3a61810c0c247e1b6fe039698eb12d3497
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-14 20:11:13 -0500

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,6 +1,111 @@
+# Import packages
 import streamlit as st
-import plotly.graph_objects as go
+import pandas as pd 
+import plotly.express as px
+import plotly.io as pio
+#from PIL import Image
+import matplotlib.pyplot as plt
+import pickle 
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+
+# Set the plotly template
+pio.templates.default = "plotly_white"
+
+# Setting the title of the tab and the favicon
+st.set_page_config(page_title='Examining Hypertension Using Health Care Data', page_icon = ':rain_cloud:', layout = 'centered')
+
+# Setting the title on the page with some styling
+st.markdown("<h1 style='text-align: center'>Examining Hypertension Using Health Care Data</h1><hr style='height:2px;border-width:0;color:gray;background-color:gray'>", unsafe_allow_html=True)
+
+# Read the data
+df = pd.read_csv("./output/df_stroke.csv")
+if st.sidebar.checkbox("Display data", False):
+    st.subheader("Show NHANES dataset")
+    st.write(df)
+
+st.sidebar.header('User Input Features')
+st.sidebar.markdown("""
+Input your data here .
+""")
+sex = df['gender']
+age = df['age']
+weight = df['weight']
+height = df['height']
+#waist_circumference = df['waist_circumference']
+systolic_bp = df['systolic_bp']
+#heart_rate = df['heart_rate']
+#BMI = df['BMI']
+#hypertension = df['hypertension']
+take_HTN_medicine = df['take_HTN_medicine']
+high_cholesterol = df['high_cholesterol']
+take_HCL_medicine = df['take_HCL_medicine']
+diabetes = df['diabetes']
+#stroke = df['stroke']
+#heart_failure = df['heart_failure']
+#CAD = df['CAD']
+#angina = df['angina']
+#heart_attack = df['heart_attack']
+sex_choice = st.sidebar.selectbox('Sex', ('Female', 'Male'))
+age_choice = st.sidebar.slider('Age', 1, 100, 30)
+weight_choice = st.sidebar.slider('Weight (lb)', 10.0, 400.0, 150.0)
+height_choice = st.sidebar.slider('Height (inch)', 10.0, 65.0, 80.0)
+#waist_circumference_choice  = st.sidebar.slider('Waist Circumference (inch)', 10.0, 80.0, 30.0)
+systolic_bp_choice = st.sidebar.slider('Blood Pressure(upper value) (mmHg)', 100.0, 250.0, 120.0)
+#heart_rate_choice = st.sidebar.slider('Heart Rate (per minute)', 30.0, 150.0, 40.0)
+#BMI_choice = st.sidebar.slider('BMI (kg/m^2)', 15.0, 70.0, 23.0)
+#hypertension_choice = st.sidebar.selectbox('Have hypertension', ('NO', 'YES'))
+take_HTN_medicine_choice = st.sidebar.selectbox('Takes BP medicines', ('NO', 'YES'))
+high_cholesterol_choice = st.sidebar.selectbox('Have high cholesterol', ('NO', 'YES'))
+take_HCL_medicine_choice = st.sidebar.selectbox('Takes cholesterol medicines', ('NO', 'YES'))
+diabetes_choice = st.sidebar.selectbox('Have diabetes', ('NO', 'YES'))
+#stroke_choice = st.sidebar.selectbox('Had any prevalent Stroke', ('NO', 'YES'))
+#heart_failure_choice = st.sidebar.selectbox('Had any heart failure', ('NO', 'YES'))
+#CAD_choice = st.sidebar.selectbox('Had any coronary heart disease', ('NO', 'YES'))
+#angina_choice = st.sidebar.selectbox('Had any angina', ('NO', 'YES'))
+#heart_attack_choice = st.sidebar.selectbox('Had any heart attack', ('NO', 'YES'))
+
+#engine_choice = st.sidebar.selectbox('', engines)
+# Creating the container for the first plot
+#with st.beta_expander('Stroke Prediction'):
+
+# Creating a selectbox dropdown with the categorical features to choose from
+#    cat_option = st.selectbox('Select a feature to examine', cat_cols, key='cat_cols1')
+
+def value(lst, string):
+    for i in range(len(lst)):
+        if lst[i] == string:
+            return i
+sex=['Female', 'Male']
+yn=['NO', 'YES']
+
+tab1, tab2 = st.tabs(["Basic Health Information", "Chronic Disease Risk"])
+  
+
+if st.sidebar.button('Submit'):
+        data = {'weight': weight_choice,
+                'height': height_choice,
+#                'BMI': BMI_choice,
+#                'waist_circumference': waist_circumference_choice,
+#                'hypertension': value(yn, hypertension_choice),
+                'take_HTN_medicine': value(yn, take_HTN_medicine_choice),
+                'high_cholesterol': value(yn, high_cholesterol_choice),
+                'take_HCL_medicine': value(yn, take_HCL_medicine_choice),
+#                'heart_rate': heart_rate_choice,
+                'systolic_bp': systolic_bp_choice,
+                'gender': value(sex, sex_choice),
+                'age': age_choice,
+                'diabetes': value(yn, diabetes_choice),
+#                'heart_failure': value(yn, heart_failure_choice),
+#                'CAD': value(yn, CAD_choice),
+#                'angina': value(yn, angina_choice),
+#                'heart_attack': value(yn, heart_attack_choice),
+#                'stroke': value(yn, stroke_choice)
+               }
+        
 def create_bmi_gauge(bmi_value):
     fig = go.Figure(go.Indicator(
         mode = "gauge+number",
@@ -21,7 +126,7 @@ def create_bmi_gauge(bmi_value):
 # BMI Meter
 st.title("BMI Meter")
 def calculate_bmi(weight, height):
-    weight_kg = weight*2.205
+    weight_kg = weight*0.454
     height_m = (height*2.54)/100
     bmi = weight_kg / (height_m ** 2)
     return bmi
