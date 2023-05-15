2920e3c5b4c51e347ba820df1b80fcd02171c84d
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-14 22:42:20 -0500

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -15,28 +15,47 @@ from sklearn.model_selection import train_test_split, cross_val_score, GridSearc
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-# Create subplots with grid layout
-fig = make_subplots(rows=2, cols=2, subplot_titles=("Person 1", "Person 2", "Person 3", "Person 4"))
+import streamlit as st
+import plotly.graph_objects as go
 
-# Define BMI data for each person
-bmi_data = [20.5, 22.1, 25.8, 23.9]
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi
 
-# Iterate over each subplot and add BMI indicator gauge
-for i, bmi in enumerate(bmi_data):
-    row = i // 2 + 1
-    col = i % 2 + 1
+def create_bmi_gauge(value):
+    fig = go.Figure(go.Indicator(
+        mode = "gauge+number",
+        value = value,
+        domain = {'x': [0, 1], 'y': [0, 1]},
+        title = {'text': "BMI"},
+        gauge = {'axis': {'range': [None, 40]},
+                 'bar': {'color': "darkblue"},
+                 'steps' : [
+                     {'range': [0, 18.5], 'color': 'lightgray'},
+                     {'range': [18.5, 24.9], 'color': 'green'},
+                     {'range': [24.9, 29.9], 'color': 'yellow'},
+                     {'range': [29.9, 40], 'color': 'red'}],
+                 'threshold' : {'line': {'color': "black", 'width': 4}, 'thickness': 0.75, 'value': value}}))
 
-    fig.add_trace(
-        go.Indicator(
-            mode="gauge+number",
-            value=bmi,
-            title="BMI",
-            domain={'x': [0, 1], 'y': [0, 1]},
-            gauge={'axis': {'visible': False}},
-        ),
-        row=row, col=col
-    )
+    return fig
 
-# Update layout and display the plot
-fig.update_layout(height=600, width=800, title="BMI Indicators")
-fig.show()
+# Streamlit App
+st.title("BMI Indicator Gauges")
+
+weight = st.number_input("Enter your weight (in kg):", min_value=0.0)
+height = st.number_input("Enter your height (in meters):", min_value=0.0)
+
+if st.button("Submit"):
+    bmi = calculate_bmi(weight, height)
+    st.write("Your BMI:", bmi)
+
+    # Create a grid of BMI indicator gauges
+    cols = st.number_input("Enter the number of columns:", min_value=1, value=3)
+    rows = (len(bmi) - 1) // cols + 1
+    for i in range(rows):
+        st.write("Row", i+1)
+        for j in range(cols):
+            index = i * cols + j
+            if index < len(bmi):
+                fig = create_bmi_gauge(bmi[index])
+                st.plotly_chart(fig, use_container_width=True)
