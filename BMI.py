b9df3fae05e455b7ba8136a18bb0f8845c977d47
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-14 19:23:23 -0500

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -31,14 +31,4 @@ height = st.number_input("Enter your height (in meters):", min_value=0.0)
 if st.button("Submit"):
     bmi = calculate_bmi(weight, height)
     st.write("Your BMI:", bmi)
-
-    # Create a grid of BMI indicator gauges
-    cols = st.number_input("Enter the number of columns:", min_value=1, value=3)
-    rows = (len(bmi) - 1) // cols + 1
-    for i in range(rows):
-        st.write("Row", i+1)
-        for j in range(cols):
-            index = i * cols + j
-            if index < len(bmi):
-                fig = create_bmi_gauge(bmi[index])
-                st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
