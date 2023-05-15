7de0ac3a2f9b0bdd0ae1c10a2699f9b5eb8095a2
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-14 22:08:09 -0500

    Update Hypertension_app.py

--- Hypertension_app.py
+++ Hypertension_app.py
@@ -129,7 +129,7 @@ def delta(l, p):
 #col1, col2 = st.columns(2)
 st.metric(
     label="Risk of Stroke", 
-    value= str(risk*100) + " %", 
+    value= str(risk) + " %", 
     delta=str(delta(userData(), risk)) + " percentage points", 
     help="""
     The change in percentage points is displayed below.
