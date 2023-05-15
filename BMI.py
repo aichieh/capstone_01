092779e9cec6eb5ddcb9e109ac546325cfc5ecca
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-14 20:05:34 -0500

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import plotly.graph_objects as go
 
-def calculate_bmi(weight, height):
-    bmi = weight / (height ** 2)
-    return bmi
-# Create subplots with grid layout
-fig = make_subplots(rows=1, cols=1, subplot_titles=("Person 1"))
-
 def create_bmi_gauge(bmi_value):
     fig = go.Figure(go.Indicator(
         mode = "gauge+number",
@@ -24,28 +18,35 @@ def create_bmi_gauge(bmi_value):
     fig.update_layout(height=200, margin=dict(l=10, r=10, t=10, b=10))
     return fig
 
+# BMI Meter
+st.title("BMI Meter")
+def calculate_bmi(weight, height):
+    weight_kg = weight*2.205
+    height_m = (height*2.54)/100
+    bmi = weight_kg / (height_m ** 2)
+    return bmi
+
+check = st.sidebar.button('Submit')
+if(check):
+    bmi = calculate_bmi(weight, height) 
+    st.title(f'Your BMI : {bmi}')
+    if bmi <18.5:
+        st.title("You are Underweight")
+    elif bmi>= 18.5 and bmi<25:
+        st.title("You are Normal")
+    elif bmi >=25 and bmi<30:
+        st.title("You are Overweight")
+    else:
+        st.title("You are Obese")
+
 # Streamlit App
 st.title("BMI Indicator Gauges")
-
-weight = st.number_input("Enter your weight (in kg):", min_value=0.0)
-height = st.number_input("Enter your height (in meters):", min_value=0.0)
-
-if st.button("Submit"):
-    bmi = calculate_bmi(weight, height)
-    st.write("Your BMI:", bmi)
+       
+#if st.button("Submit"):
+#    bmi = calculate_bmi(weight, height)
+#    st.write("Your BMI:", bmi)
     #st.plotly_chart(fig, use_container_width=True)
     # Generate a grid of BMI gauges
-
-    fig.add_trace(
-        go.Indicator(
-            mode="gauge+number",
-            value=bmi,
-            title="BMI",
-            domain={'x': [0, 1], 'y': [0, 1]},
-            gauge={'axis': {'visible': False}},
-            ),
-            row=row, col=col
-        )
 
     # Update layout and display the plot
     fig.update_layout(height=600, width=800, title="BMI Indicators")
