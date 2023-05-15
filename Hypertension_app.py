0f955897a9da3ae6889789f8ed20b4d87a827359
AiChieh Lin <125310155+aichieh@users.noreply.github.com>
2023-05-14 21:54:16 -0500

    Update Hypertension_app.py

--- Hypertension_app.py
+++ Hypertension_app.py
@@ -118,19 +118,19 @@ def userData():
 @st.cache(allow_output_mutation=True)
 def delta(l, p):
     if len(l) == 0:
-        l.extend([0, round(p*100, 1)])
+        l.extend([0, round(p, 1)])
         d = 0
     else:
         l.pop(0)
-        l.append(round(p*100, 1))
+        l.append(round(p, 1))
         d = l[1] - l[0]
     return d
 
 #col1, col2 = st.columns(2)
 st.metric(
     label="Risk of Stroke", 
-    value= str(risk) + " %", 
-    delta=str(delta(userData(), risk)) + " percentage points", 
+    value= str(risk, 1) + " %", 
+    delta=str(delta(userData(), risk,2)) + " percentage points", 
     help="""
     The change in percentage points is displayed below.
     """,
